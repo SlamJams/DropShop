764c2f3367ca8e836dbdc2bac01b65f4ed900752
Brian <brian2najera@gmail.com>
2021-04-02 22:48:32 -0500

    Implemented Repository pattern for Products

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -20,11 +21,19 @@ namespace API
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        /* NOTE: 
+            In .NET's dependency injection there are three major lifetimes:
+                Singleton which creates a single instance throughout the application. It creates the instance for the first time and reuses the same object in the all calls.
+                Scoped lifetime services are created once per request within the scope. It is equivalent to a singleton in the current scope. For example, in MVC it creates one instance for each HTTP request, but it uses the same instance in the other calls within the same web request.
+                Transient lifetime services are created each time they are requested. This lifetime works best for lightweight, stateless services.
+                Soruce:https://stackoverflow.com/questions/38138100/addtransient-addscoped-and-addsingleton-services-differences
+        */
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
