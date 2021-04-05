d0b9783f91d758277af4f775a50d124ef7939277
Brian <brian2najera@gmail.com>
2021-04-05 17:02:02 -0500

    Added code to apply migrations and create database at application startup

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var hostBulder = CreateHostBuilder(args).Build();
+           using(var host = hostBulder.Services.CreateScope())
+           {
+               var services = host.ServiceProvider;
+               var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+               try
+               {
+                   var context = services.GetRequiredService<StoreContext>();
+                   await context.Database.MigrateAsync();
+               }
+               catch(Exception ex)
+               {
+                   var logger = loggerFactory.CreateLogger<Program>();
+                   logger.LogError(ex, "Migration failed during startup");
+               }
+               
+               hostBulder.Run();
+           }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
